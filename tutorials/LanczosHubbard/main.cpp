07c5a6c17716b334d0c7873802100f9a2b053fac
Hao Shi <boruoshihao@gmail.com>
2016-12-06 16:40:44 -0500

    add a timer.

--- tutorials/LanczosHubbard/main.cpp
+++ tutorials/LanczosHubbard/main.cpp
@@ -26,6 +26,9 @@ int main(int argc, char** argv)
 {
     MPIInitFunnel(argc,argv);
 
+    TimerHao timer;
+    timer.start(); if(MPIRank()==0) {cout<<"\nStart time:\n";timer.printCurrentTime();}
+
 #ifdef USE_MAGMA
     magma_init();
 #endif
@@ -35,6 +38,9 @@ int main(int argc, char** argv)
 #ifdef USE_MAGMA
     magma_finalize();
 #endif
+
+    timer.end(); if(MPIRank()==0) {cout<<"\n\nEnd time:\n";timer.printCurrentTime();}
+    if(MPIRank()==0) {cout<<"The program's running walltime is:\n"; timer.printFormat();}
 
     MPIFinalize();
     return 0;
