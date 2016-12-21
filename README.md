cf371433605b7855c1e46c9921be3145dd38c9e2
Hao Shi <boruoshihao@gmail.com>
2016-12-21 11:20:44 -0500

    Use template in readWriteHao.

--- README.md
+++ README.md
@@ -119,10 +119,6 @@ Then see tutorials for detail examples.
 - `FFTW` wrap does not support 64 bit int and openmp.
 - `MAGMA` wrap does not support 64 bit int and openmp.
 
-  //TODO: readWrite library ==> Use template!
-
-  //TODO: Add supercubic library.
-
   //TODO: Benchmark with Ettore's data, imaginary time and spectral function.
 
   //TODO: Read and Write use HDF5 file? For exact diagonal code, it should be good.
--- libhao/readWriteHao/include/readWriteHao.h
+++ libhao/readWriteHao/include/readWriteHao.h
@@ -5,30 +5,134 @@
 #ifndef AFQMCLIB_READWRITEHAO_H
 #define AFQMCLIB_READWRITEHAO_H
 
+#include <fstream>
+#include <iomanip>
 #include "../../mpiHao/include/mpi_fun.h"
 
 bool checkFile (const std::string &filename);
 void removeFile(const std::string &filename);
 
-void writeFile(size_t data, const std::string &filename);
-void readFile(size_t &data, const std::string &filename);
+template<class T> void writeFile(T data, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    file<<std::setw(26)<<data<<"\n";
+    file.close();
+}
 
-void writeFile(double data, const std::string &filename);
-void readFile(double &data, const std::string &filename);
+template<class T> void readFile(T &data, const std::string &filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file >> data;
+    file.close();
+}
 
-void writeFile(size_t L, const double *data, const std::string &filename);
-void readFile(size_t L, double *data, const std::string& filename);
+template<class T> void writeFile(std::complex<T> data, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    file<<std::setw(26)<<data.real()<<std::setw(26)<<data.imag()<<"\n";
+    file.close();
+}
 
-void writeFile(size_t L, const double *dataOne, const double *dataTwo, const std::string &filename);
-void readFile(size_t L, double *dataOne, double *dataTwo, const std::string &filename);
+template<class T> void readFile(std::complex<T> &data, const std::string &filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    T real, imag;
+    file >> real >> imag;
+    data = std::complex<T>(real, imag);
+    file.close();
+}
 
-void writeFile(std::complex<double> data, const std::string &filename);
-void readFile(std::complex<double> &data, const std::string &filename);
+template<class T> void writeFile(size_t L, const T *data, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    for(size_t i = 0; i < L; ++i) file<<std::setw(26)<<data[i]<<"\n";
+    file.close();
+}
 
-void writeFile(size_t L, const std::complex<double> *data, const std::string &filename);
-void readFile(size_t L, std::complex<double> *data, const std::string &filename);
+template<class T>  void readFile(size_t L, T *data, const std::string& filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    for(size_t i = 0; i < L; ++i) file>>data[i];
+    file.close();
+}
 
-void writeFile(size_t L, const std::complex<double> *dataOne, const std::complex<double> *dataTwo, const std::string &filename);
-void readFile(size_t L, std::complex<double> *dataOne, std::complex<double> *dataTwo, const std::string &filename);
+template<class T> void writeFile(size_t L, const T *dataOne, const T *dataTwo, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    for(size_t i = 0; i < L; ++i) file<<std::setw(26)<<dataOne[i]<<std::setw(26)<<dataTwo[i]<<"\n";
+    file.close();
+}
+
+template<class T> void readFile(size_t L, T *dataOne, T *dataTwo, const std::string &filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    for(size_t i = 0; i < L; ++i) file>>dataOne[i]>>dataTwo[i];
+    file.close();
+}
+
+template<class T> void writeFile(size_t L, const std::complex<T> *data, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    for(size_t i = 0; i < L; ++i) file<<std::setw(26)<<data[i].real()<<std::setw(26)<<data[i].imag()<<"\n";
+    file.close();
+}
+
+template<class T> void readFile(size_t L, std::complex<T> *data, const std::string &filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    T real, imag;
+    for(size_t i = 0; i < L; ++i) { file>>real>>imag; data[i] = std::complex<T>(real, imag); }
+    file.close();
+}
+
+template<class T> void writeFile(size_t L, const std::complex<T> *dataOne, const std::complex<T> *dataTwo, const std::string &filename)
+{
+    std::ofstream file;
+    file.open(filename, std::ios::out|std::ios::app);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    file<<std::setprecision(16)<<std::scientific;
+    for(size_t i = 0; i < L; ++i) file<<std::setw(26)<<dataOne[i].real()<<std::setw(26)<<dataOne[i].imag()
+                                      <<std::setw(26)<<dataTwo[i].real()<<std::setw(26)<<dataTwo[i].imag()<<"\n";
+    file.close();
+}
+
+template<class T> void readFile(size_t L, std::complex<T> *dataOne, std::complex<T> *dataTwo, const std::string &filename)
+{
+    std::ifstream file;
+    file.open(filename, std::ios::in);
+    if ( ! file.is_open() ) {std::cout << "Error opening file in File!!! "<<filename<<std::endl; exit(1);}
+    T real, imag;
+    for(size_t i = 0; i < L; ++i)
+    {
+        file>>real>>imag; dataOne[i] = std::complex<T>(real, imag);
+        file>>real>>imag; dataTwo[i] = std::complex<T>(real, imag);
+    }
+    file.close();
+}
 
 #endif //AFQMCLIB_READWRITEHAO_H
